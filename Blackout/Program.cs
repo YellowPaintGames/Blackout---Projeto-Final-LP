2b00293fde2241801ab7968bf6eb56673140d885
Francisco Caldeira <kikocaldeira2007@gmail.com>
2026-05-18 20:35:50 +0100

    Create project

---
+++ Blackout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blackout
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
